a613830acadb6b88b00972bc16c39bec9cf502f2
nikolai <n.ivanov.p@gmail.com>
2017-08-19 12:35:54 +0300

    changed ssh command

--- rdocker-ssh-tunnel.sh
+++ rdocker-ssh-tunnel.sh
@@ -45,8 +45,9 @@ control_path="$HOME/.rdocker-master-$(date +%s%N)"
 
 #ssh "${remote_host}" -p ${SSH_PORT} -i "$ssh_key_file" -nNf -o "StrictHostKeyChecking no" -o ControlMaster=yes -o ControlPath="${control_path}" -o ControlPersist=yes
 
-ssh -i "$ssh_key_file" -fNL ${local_port}:localhost:${local_port} -p ${SSH_PORT} "${remote_host}" -o "StrictHostKeyChecking no"
-export DOCKER_HOST="tcp://localhost:${local_port}"
+#ssh -i "$ssh_key_file" -fNL ${local_port}:localhost:${local_port} -p ${SSH_PORT} "${remote_host}" -o "StrictHostKeyChecking no"
+ssh -i "$ssh_key_file" -p ${SSH_PORT} "${remote_host}" -o "StrictHostKeyChecking no"
+#export DOCKER_HOST="tcp://localhost:${local_port}"
 
 if [[ -n "$command" ]]; then
     bash -c "$command"
